2954af131af9e36396c92e9d699992bcbc258510
lerosua <lerosua@gmail.com>
2019-04-08 18:53:19 +0800

    fix

--- FileBrowser/PreviewTransitionViewController.swift
+++ FileBrowser/PreviewTransitionViewController.swift
@@ -18,7 +18,7 @@ class PreviewTransitionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.addChildViewController(quickLookPreviewController)
+        self.addChild(quickLookPreviewController)
         containerView.addSubview(quickLookPreviewController.view)
         quickLookPreviewController.view.frame = containerView.bounds
         quickLookPreviewController.didMove(toParentViewController: self)
